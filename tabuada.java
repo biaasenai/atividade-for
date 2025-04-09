10f7c60fc5da30ffd612e9489296e8d5cec5dd75
unknown <cvieira.bia@gmail.com>
2025-04-09 11:17:23 -0300

    FOR

---
+++ tabuada.java
@@ -0,0 +1,34 @@
+package FOR;
+import java.util.Scanner;
+public class tabuada {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int numero;
+		Scanner ler = new Scanner(System.in);
+		System.out.println("Insira um número para ver a tabuada: ");
+		numero = ler.nextInt();
+		
+		for (int i = 1; i <=10; i++) {
+			System.out.println(i + "X" + numero + " = " + (i*numero));
+		
+			
+			ler.close();
+		}
+	}}
+			
+			
+		
+			
+			
+			
+		
+		
+		
+		
+		
+
+	
+
+
